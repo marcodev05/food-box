20b0c64e362b9d72286ebd9a091724ee1efea070
marcodev05 <maminiainamarco@gmail.com>
2022-08-11 13:45:27 +0300

    init food delivery project

---
+++ src/main/java/com/tsk/foodbox/FoodBoxApplication.java
@@ -0,0 +1,13 @@
+package com.tsk.foodbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodBoxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tsk/foodbox/FoodBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsk.foodbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
